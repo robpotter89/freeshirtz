b57903df79053a46add8c1ae65fad00bb7e0724a
Mathieu Robinson <MRobinson@markettrack.com>
2019-10-01 16:40:22 -0400

    Implement machine learning shirt acquiry solution

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
+import tensorflow as tf
+
 NUM_SHIRTS = 69
 SHIRT_MESSAGE = "Gimme shirt pls"
 
-def main():
-    for i in range(NUM_SHIRTS):
-        print(SHIRT_MESSAGE)
+
+def brute_force_shirt(num_shirts=NUM_SHIRTS, shirt_message=SHIRT_MESSAGE):
+    for i in range(num_shirts):
+        print(shirt_message)
+
+
+def ml_shirt_acquiry():
+    shirt = tf.constant("Gimme a t-shirt")
+    sess = tf.Session()
+    return sess.run(shirt)
+
 
 if __name__ == '__main__':
-    main()
-
+    print(ml_shirt_acquiry())
