278456d067e927519df416a1e26cd3d669f89763
Mathieu Robinson <MRobinson@markettrack.com>
2019-10-01 16:31:07 -0400

    Update main with more robust shirt acquiry solution

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+NUM_SHIRTS = 69
+SHIRT_MESSAGE = "Gimme shirt pls"
+
+def main():
+    for i in range(NUM_SHIRTS):
+        print(SHIRT_MESSAGE)
+
 if __name__ == '__main__':
-    print("gimme shirt")
+    main()
 
